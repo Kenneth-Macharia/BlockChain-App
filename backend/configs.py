88cd9d4c15474684d42581154c9735a1ceb0f015
Kenneth Macharia <kenneth.macharia@live.com>
2020-07-11 16:28:50 +0300

    fixes db authentication

--- backend/configs.py
+++ backend/configs.py
@@ -9,5 +9,5 @@ user = os.getenv('DB_USER')
 password = os.getenv('DB_PASSWORD')
 host = os.getenv('DB_HOST')
 
-MONGO_URI = 'mongodb://%s:%s@%s/blockchain_db' % (
+MONGO_URI = 'mongodb://%s:%s@%s/blockchain_db?authSource=admin' % (
     user, password, host)
