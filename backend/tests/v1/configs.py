c329f511bee13493a5dd87078da87c5c7b5f7375
Kenneth Macharia <kenneth.macharia@live.com>
2020-10-09 17:31:25 +0300

    adds redis caching tests

--- backend/tests/v1/configs.py
+++ backend/tests/v1/configs.py
@@ -1,6 +1,7 @@
 '''Test DB configs'''
 
-from ...configs import muser, mpassword, mhost, api_key, init_node
+from ...configs import (muser, mpassword, mhost, api_key, init_node,
+                        ruser, rpassword, rhost)
 
-MONGO_URI = 'mongodb://%s:%s@%s/test_db' % (
-    muser, mpassword, mhost)
+MONGO_URI = 'mongodb://%s:%s@%s/test_db' % (muser, mpassword, mhost)
+REDIS_URL = 'redis://%s:%s@%s:6379/0' % (ruser, rpassword, rhost)
