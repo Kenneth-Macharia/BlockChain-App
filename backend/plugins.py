a0664cab054171b6b9f5231f1c1d37dc5432495b
Kenneth Macharia <kenneth.macharia@live.com>
2020-11-12 12:37:19 +0300

    removes backend code

--- backend/plugins.py
+++
@@ -1,7 +0,0 @@
-'''This module initializes all app plugins such databases'''
-
-from flask_pymongo import PyMongo
-from flask_redis import FlaskRedis
-
-mongo = PyMongo()
-redis_client = FlaskRedis()
--- backend/tests/v1/configs.py
+++
@@ -1,7 +0,0 @@
-'''Test DB configs'''
-
-from ...configs import (muser, mpassword, mhost, api_key, init_node,
-                        ruser, rpassword, rhost)
-
-MONGO_URI = 'mongodb://%s:%s@%s/test_db' % (muser, mpassword, mhost)
-REDIS_URL = 'redis://%s:%s@%s:6379/0' % (ruser, rpassword, rhost)
